51f9ea8c9762376cc042a1ea3f8fb297a545d6a5
Ankit Chauhan <sms.ankit@gmail.com>
2025-01-31 16:23:12 +0530

    Update products.service.ts

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -18,8 +18,8 @@ export class ProductsService {
   }
 
   public getProducts(): Observable<Product> {
-    // const procust_url = 'https://fakestoreapi.com/products';
-    const procust_url =  'http://4.224.109.85:8080/Products1';
+   const procust_url = 'https://fakestoreapi.com/products';
+    //const procust_url =  'http://4.224.109.85:8080/Products1';
     // const procust_url = 'https://productapiwithapim-f6fbghgudmbnffh9.centralindia-01.azurewebsites.net/Products1';
     // return this.http.get<Product>(`${environment.productApiUrl}/${this.productUrl}`);
     return this.http.get<Product>(`${procust_url}`);
