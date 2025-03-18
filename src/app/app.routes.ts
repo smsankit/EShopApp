8e6a6cba58454f0dbc45a305dba9eabe126c8ba3
smsankit <sms.ankit@gmail.com>
2025-03-18 13:31:10 +0530

    Add Contact Us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthPageComponent } from './auth-page/auth-page.component'
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 export const routes: Routes = [
     { path: '', component: AuthPageComponent },
     { path: 'productDetails', component: ProductDetailsComponent },
-    { path: 'home', component: LandingpageComponent }
+    { path: 'home', component: LandingpageComponent },
+    { path: 'contact', component: ContactusComponent }
 ];
 
   @NgModule({
