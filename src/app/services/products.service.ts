f0d6a1e52dbbb1775d31a712701a65a941d10b3c
smsankit <sms.ankit@gmail.com>
2025-03-24 10:45:20 +0530

    Update product endpoint

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -19,12 +19,9 @@ export class ProductsService {
 
   public getProducts(): Observable<Product> {
     const product_endpoint = 'products';
-    // const procust_url = 'https://fakestoreapi.com/products';
     // const product_url = 'http://amcart-api.centralindia.cloudapp.azure.com/products1';
-    // ${environment.authApiUrl}/${this.registerUrl}
-    // return this.http.get<Product>(`${product_url}`);
-    // return this.http.get<Product>(`${environment.productApiUrl}/${product_endpoint}`);
-    return this.http.get<Product>('https://fakestoreapi.com/products');
+    return this.http.get<Product>(`${environment.productApiUrl}/${product_endpoint}`);
+    // return this.http.get<Product>('https://fakestoreapi.com/products');
 
   }
 }
